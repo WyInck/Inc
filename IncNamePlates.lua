0a3ecaa736d71a05cc854896a822dc125ecc3291
Inc <38887202+WyInck@users.noreply.github.com>
2018-08-29 22:26:46 +0800

    Create IncNamePlates.lua

---
+++ IncNamePlates.lua
@@ -0,0 +1,38 @@
+--IncNameplates
+hooksecurefunc("CompactUnitFrame_OnUpdate", function(frame)
+if C_NamePlate.GetNamePlateForUnit(frame.unit) ~= C_NamePlate.GetNamePlateForUnit("player") and not UnitIsPlayer(frame.unit) and not CompactUnitFrame_IsTapDenied(frame) then
+   local threat = UnitThreatSituation("player", frame.unit) or 0
+   local reaction = UnitReaction(frame.unit, "player")
+   local name = UnitName(frame.unit)
+   local colorC = _G["RAID_CLASS_COLORS"][select(2, UnitClass("player"))]
+    if name == "爆炸物" then
+      r, g, b = 0.8, 0, 1
+	  -- 特殊单位，紫红
+	  elseif name == "戈霍恩之嗣" then
+      r, g, b = 0.8, 0, 1
+	  -- 特殊单位，紫红
+	  elseif UnitIsUnit(frame.displayedUnit, "target") then
+      r, g, b = colorC.r, colorC.g, colorC.b
+	  -- 特殊单位，职业颜色
+    elseif threat == 3 then
+r, g, b = 0, 1, 1
+      -- 仇恨最高，天蓝
+    elseif threat == 2 and GetSpecializationRole(GetSpecialization()) == "TANK" then
+r, g, b = 1, 0.2, 1
+      -- 坦克职责仇恨不稳（坦克仇恨正在减少），紫红
+    elseif threat == 2 then
+r, g, b = 1, 0.2, 1
+      -- 非坦克职责仇恨不稳（仇恨大于坦克且正在减少），粉色
+    elseif threat == 1 then
+r, g, b = 1, 0.5, 0
+      -- 仇恨较高，橙色
+	  elseif reaction == 4 then
+r, g, b = 1, 1, 0
+	  -- 仇恨中立，黄色
+else
+r, g, b = 1, 0, 0
+      -- 其他，红色
+end
+frame.healthBar:SetStatusBarColor(r, g, b, 1)
+end
+end) 
