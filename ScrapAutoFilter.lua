ce5eb07a42d9f5613899ddfe507ad9202f50b264
Inc <38887202+WyInck@users.noreply.github.com>
2019-11-21 12:14:00 +0800

    Update ScrapAutoFilter.lua

--- ScrapAutoFilter.lua
+++ ScrapAutoFilter.lua
@@ -1,15 +1,22 @@
 -- Scrap
 local Scrap = CreateFrame("Frame") 
 	Scrap:SetScript("OnEvent", function(...) 
-	for b = 0, 4 do 
-		for s = 1,GetContainerNumSlots(b) do 
-        	local l = GetContainerItemLink(b, s) 
-			if l and select(3, GetItemInfo(l)) < 3 then
-      			-- the quality of the scrap item '< 3', its the green done
-               			UseContainerItem(b, s) 
-            		end 
-        	end 
+	for bag = 0, 4 do 
+		for slot = 1, GetContainerNumSlots(bag) do 
+        	local link = GetContainerItemLink(bag, slot) 
+			if link then
+				local quality = select(3, GetItemInfo(link))
+				if quality then
+					if  quality < 3 then 
+					-- the quality of the scrap item '< 3', its the green done
+						UseContainerItem(bag, slot) 
+					end 
+				end
+			end
+        end 
    	 end 
 end) 
 Scrap:RegisterEvent("SCRAPPING_MACHINE_PENDING_ITEM_CHANGED")
 Scrap:RegisterEvent("SCRAPPING_MACHINE_SCRAPPING_FINISHED")
+Scrap:RegisterEvent("BAG_OPEN")
+
